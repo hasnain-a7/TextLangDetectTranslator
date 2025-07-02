1dc959168e874197c1546fccfc35def84c78001e
hasnain-a7 <husnainahmed519@gmail.com>
2025-07-03 01:29:34 +0500

    final touch

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ LANGUAGES = {
     "Urdu": "ur",
     "Persian": "fa"
 }
+
 @app.route('/')
 def home():
     return "Backend is running!"
@@ -43,5 +44,5 @@ def detect_and_translate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Get port from env, default 5000
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
