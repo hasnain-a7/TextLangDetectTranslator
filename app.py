886b37d4cd04943c1a11422ccf7eedaed8f8e2f4
hasnain-a7 <husnainahmed519@gmail.com>
2025-07-03 01:21:09 +0500

    Initial commit

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def detect_and_translate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 5000))  # Get port from env, default 5000
+    app.run(host='0.0.0.0', port=port)
